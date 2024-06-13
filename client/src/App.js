1e26d3c342b12244a566f43b65a7546166db433b
viktor <viktorerdos52@gmail.com>
2024-06-13 18:06:12 +0200

    textDecor-off Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Register from "./components/Register.js";
 import Login from "./components/Login.js";
-import TodoInterface from './components/todo-interface.js'
+import TodoInterface from './components/todo-interface.js';
 
 function App() {
   return (
@@ -25,9 +25,9 @@ function Home() {
     <div>
         <nav>
           <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Registration</Link></li>
-            <li><Link to="todo">Join as guest</Link></li>
+            <li><Link to="/login" style={{ textDecoration: 'none'}}>Login</Link></li>
+            <li><Link to="/register" style={{ textDecoration: 'none'}}>Registration</Link></li>
+            <li><Link to="todo" style={{ textDecoration: 'none'}}>Join as guest</Link></li>
           </ul>
         </nav>
       </div>
